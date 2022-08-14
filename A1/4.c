49a62143a4fd67ca5c9ab90cf511342d18983328
vijay968 <vijaykumar719585@gmail.com>
2022-08-14 21:23:08 +0530

    deleted

--- A1/4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int r;
-    printf("Enter the radius");
-    scanf("%d" ,&r);\
-    printf("Area of circle is %f having radius %d",3.14*r*r,r);
-    return 0;
-}
--- A2/6.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    char a;
-    printf("Ente a character :");
-    scanf("%c",&a);
-    printf("ASCII code is %d",a);
-    return 0;
-}
